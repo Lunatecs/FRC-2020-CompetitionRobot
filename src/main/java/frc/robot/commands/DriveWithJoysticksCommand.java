600614b1e9f263f9b156e5173f30a0ac3cfe02f4
sujitlunatecsemail316 <58493281+sujitlunatecsemail316@users.noreply.github.com>
2020-01-25 11:03:05 -0500

    Added code for DriveWithJoysticksCommand

--- src/main/java/frc/robot/commands/DriveWithJoysticksCommand.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticksCommand.java
@@ -7,14 +7,24 @@
 
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class DriveWithJoysticksCommand extends CommandBase {
+  private final DrivetrainSubsystem driveTrain;
+  private final DoubleSupplier speed;
+  private final DoubleSupplier rotation;
+
   /**
-   * Creates a new DriveWithJoysticksCommand.
+   * Creates a new DriveWithJoystick.
    */
-  public DriveWithJoysticksCommand() {
+  public DriveWithJoysticksCommand(DrivetrainSubsystem driveTrain, DoubleSupplier speed, DoubleSupplier rotation) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.driveTrain = driveTrain;
+    this.speed = speed;
+    this.rotation = rotation;
+    addRequirements(driveTrain);
   }
 
   // Called when the command is initially scheduled.
@@ -25,6 +35,11 @@ public class DriveWithJoysticksCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    if (Math.abs(speed.getAsDouble()) > 0.15 || Math.abs(rotation.getAsDouble()) > 0.15) {
+      driveTrain.arcadeDrive((speed.getAsDouble()), rotation.getAsDouble());
+    }else{
+      driveTrain.arcadeDrive(0,0);
+    }
   }
 
   // Called once the command ends or is interrupted.
