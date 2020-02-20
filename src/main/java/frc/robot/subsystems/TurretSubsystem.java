606df1cac56f450538583d009c87c5ce869042d0
wpohlhau <william.pohlhaus@villanova.edu>
2020-02-19 19:55:15 -0500

    Begin to add pid top speed to turrent subsystem

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -7,28 +7,36 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.TurretConstants;
 
 public class TurretSubsystem extends SubsystemBase {
 
-  private final WPI_TalonSRX turret = new WPI_TalonSRX(TurretConstants.Turret_ID);
+  private final TalonSRX turret = new TalonSRX(TurretConstants.Turret_ID);
   private NeutralMode TURRET_NEUTRALMODE = NeutralMode.Brake;
-
+  private PIDController pidController;
   /**
    * Creates a new TurretSubsystem.
    */
   public TurretSubsystem() {
     turret.configFactoryDefault();
     turret.setNeutralMode(TURRET_NEUTRALMODE);
+    resetPostion();
+    pidController = new PIDController(.001,0,0);
+    pidController.setSetpoint(10000);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Turret Encoder", getPostion());
   }
 
   /**
@@ -36,6 +44,15 @@ public class TurretSubsystem extends SubsystemBase {
    * @param speed
    */
   public void setTurretSpeed(double speed) {
+    pidController.calculate(this.getPostion());
     turret.set(ControlMode.PercentOutput, speed);
+  }
+
+  public int getPostion() {
+    return this.turret.getSelectedSensorPosition(0);
+  }
+
+  public void resetPostion() {
+    turret.setSelectedSensorPosition(0, 0, 10);
   }
 }
